3cc12c07fddaf6468ea2e198fbf1b956d45c67c0
Christian Frisson <christian.frisson@gmail.com>
2018-08-13 12:04:00 -0600

    Use sync gitRemoteOriginUrl call

--- index.js
+++ index.js
@@ -8,31 +8,29 @@ var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ");
 var formatTime = d3.timeFormat("%B %e, %Y");
 
 module.exports = (ast) => {
-    remoteOriginUrl(function (err, url) {
-        var info = getRepoInfo();
-        let elements = [];
-        elements.push(AST.createNode('span', { id: 'revisionTitle' }, ['Revision: ']));
-        let commitUrl = '';
-        if (err) {
-            console.log(err);
-            url = '';
+    var url = remoteOriginUrl.sync();
+    var info = getRepoInfo();
+    let elements = [];
+    elements.push(AST.createNode('span', { id: 'revisionTitle' }, ['Revision: ']));
+    let commitUrl = '';
+    if (url === undefined) {
+        url = '';
+    }
+    else {
+        console.log('url', url);
+        url = url.split('.git')[0];
+        if (!url.includes("http") && url.includes("git@")) {
+            url = "https://" + url.split('git@')[1].replace(':', '/');
         }
-        else {
-            console.log(url);
-            url = url.split('.git')[0];
-            if (!url.includes("http") && url.includes("git@")) {
-                url = "https://" + url.split('git@')[1].replace(':', '/');
-            }
-            commitUrl = url + '/commit/' + info.sha;
-        }
-        elements.push(AST.createNode('a', { href: commitUrl, id: 'revisionMessage' }, [info.commitMessage]));
-        let author = info.committer.split('<')[0];
-        elements.push(AST.createNode('span', { id: 'revisionAuthor' }, [' by ' + author]));
-        elements.push(AST.createNode('span', { id: 'revisionDate' }, [' on ' + formatTime(parseTime(info.committerDate))]));
-        let revisionDiv = AST.createNode('div', { id: 'revisionDiv' }, elements);
-        let ASTwithRevision = AST.modifyNodesByName(ast, 'Revision', (node) => {
-            return revisionDiv;
-        });
-        return ASTwithRevision;
+        commitUrl = url + '/commit/' + info.sha;
+    }
+    elements.push(AST.createNode('a', { href: commitUrl, id: 'revisionMessage' }, [info.commitMessage]));
+    let author = info.committer.split('<')[0];
+    elements.push(AST.createNode('span', { id: 'revisionAuthor' }, [' by ' + author]));
+    elements.push(AST.createNode('span', { id: 'revisionDate' }, [' on ' + formatTime(parseTime(info.committerDate))]));
+    let revisionDiv = AST.createNode('div', { id: 'revisionDiv' }, elements);
+    let ASTwithRevision = AST.modifyNodesByName(ast, 'Revision', (node) => {
+        return revisionDiv;
     });
+    return ASTwithRevision;
 };
